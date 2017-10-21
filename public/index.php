0b4aa5d524d7beefda1203d9f0a7d633a9146805
Teodor Savin <teodor@aceandtate.com>
2017-10-21 11:24:24 +0200

    headers

--- public/index.php
+++ public/index.php
@@ -13,7 +13,7 @@
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST');
 header('Access-Control-Allow-Headers: Content-Type');
-header('Content-type: application/json; charset=utf-8');
+//header('Content-type: application/json; charset=utf-8');
 
 $app = require __DIR__.'/../bootstrap/app.php';
 
